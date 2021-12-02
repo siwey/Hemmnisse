6a39b671ff311215c2b9ba016b40fe5111be2d87
Simon Wey <simonwey@Simons-MacBook-Pro.local>
2021-12-02 23:17:20 +0100

    Compare Upload

---
+++ Compare_FK_Mang.R
@@ -0,0 +1,46 @@
+rm(list=ls())
+
+setwd("/Users/simonwey/Repos/Hemmnisse")
+
+library(echarts4r)
+library(viridisLite)
+library(jsonlite)
+library(tidyverse)
+library(readxl)
+
+Dat="2011 Q1"
+
+
+xlsx_exa <- read_xlsx("20201112_BeschBar_Hemmn_FK.xlsm", sheet="Hemmnisse_Calc") #,range ="" "d19:d44")
+xlsx_exa <- xlsx_exa[c(141,150:193),]
+xlsx_exa[1,1] <- "Date"
+names(xlsx_exa) <- xlsx_exa[1,]
+
+xlsx_exa <- xlsx_exa[-1,]
+
+
+V20 <- c(as.numeric(tail(xlsx_exa$Grosshandel,n=1)),as.numeric(tail(xlsx_exa$Gastgewerbe,n=1)),as.numeric(tail(xlsx_exa$IT,n=1)),as.numeric(tail(xlsx_exa$Bau,n=1)), as.numeric(tail(xlsx_exa$MEM,n=1)), as.numeric(tail(xlsx_exa$Pharma,n=1)))
+V21 <- c(mean(as.numeric(xlsx_exa$Grosshandel)),mean(as.numeric(xlsx_exa$Gastgewerbe)),mean(as.numeric(xlsx_exa$IT)),mean(as.numeric(xlsx_exa$Bau)),mean(as.numeric(xlsx_exa$MEM)),mean(as.numeric(xlsx_exa$Pharma)))
+names <- c(as.character('Grosshandel'),as.character('Gastgewerbe'),as.character('IT'),as.character('Bau'),as.character('MEM-Industrie'),as.character('Pharma'))
+V21
+
+d <- data.frame(
+  nam=names,
+  act_val=V20,
+  mean_val=V21
+)
+
+d |>
+  e_charts(nam) |>
+  e_bar(act_val, name="2021:Q4") |>
+  e_bar(mean_val, name="Mittelwert 2011 bis 2021:Q3") |>
+ # e_line(Banken) |>
+ # e_line(Gastgewerbe) |>
+ # e_line(Gesundheit) |>
+ # e_line(IT) |>
+  e_title("Arbeitskräftemangel",subtext="Saisonbereinigte Werte",sublink="www.focus50plus.ch", left="10%") |>
+  e_tooltip(trigger="axis") |>
+  #e_datazoom(type = "slider") |>
+  e_legend(orient = 'horizontal', left = 460, top = 10) |>
+  e_format_y_axis(suffix = "%") |>
+  e_toolbox_feature("saveAsImage")
