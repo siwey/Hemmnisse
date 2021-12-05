71c5f3b0fa5c849cbfc301e309a05092f4063af4
Simon Wey <simonwey@Simons-MacBook-Pro.local>
2021-12-05 12:43:22 +0100

    commit 5.12.21

--- Hemmn_branches.R
+++ Hemmn_branches.R
@@ -21,24 +21,32 @@ xlsx_exa_No_probl <- xlsx_exa[c(211:263),]
 xlsx_exa_oth_probl <- xlsx_exa[c(281:333),]
 xlsx_exa_tech_Kap <- xlsx_exa[c(351:403),]
 TS_Demand <- c()
+TS_mean_Demand <- c()
 TS_Fin_Sit <- c()
+TS_mean_Fin_Sit <- c()
 TS_M_AK <- c()
+TS_mean_M_AK <- c()
 TS_No_probl <- c()
+TS_mean_No_probl <- c()
 TS_oth_probl <- c()
+TS_mean_oth_probl <- c()
 TS_tech_Kap <- c()
+TS_mean_tech_Kap <- c()
+
 
 for(i in 1:length(Tables)) { 
   eval(parse(text=paste(paste0(paste0("xlsx_exa_", Tables[i]),"[1,1]"),"<-'Date'")))
   eval(parse(text=paste(paste0(paste0(paste0("names(xlsx_exa_", Tables[i]),")"), "<- eval(parse(text=paste0(paste0('xlsx_exa_', Tables[i]),'[1,]')))"))))
   eval(parse(text=paste(paste0(paste0("xlsx_exa_", Tables[i]), "<- eval(parse(text=paste0(paste0('xlsx_exa_', Tables[i]),'[-1,]')))"))))
 }
-
+mean(as.numeric(xlsx_exa_Demand$Bau),n=2)
 for (j in 1:length(Tables)) {
-for(i in 1:length(Branches)) { 
-  #eval(parse(text=paste(paste0("TS_", Tables[j]),"<-'Date'")))
-  eval(parse(text=paste(paste0("TS_", Tables[j]), "<- eval(parse(text=paste0(paste0(paste0(paste0(paste0(paste0('c(TS_',Tables[j]),',tail(xlsx_exa_'), Tables[j]),'$'),Branches[i]),',n=1))')))")))
-  
-}
+  for(i in 1:length(Branches)) { 
+    #Last value of the time series obstacles
+    eval(parse(text=paste(paste0("TS_", Tables[j]), "<- eval(parse(text=paste0(paste0(paste0(paste0(paste0(paste0('c(TS_',Tables[j]),',tail(xlsx_exa_'), Tables[j]),'$'),Branches[i]),',n=1))')))")))
+    #Mean of the time series obstacles
+    eval(parse(text=paste(paste0("TS_mean_", Tables[j]), "<- eval(parse(text=paste0(paste0(paste0(paste0(paste0(paste0('c(TS_mean_',Tables[j]),',mean(as.numeric(xlsx_exa_'), Tables[j]),'$'),Branches[i]),')))')))")))
+  }
 }
 a <- unlist(Branches)
 a
@@ -54,9 +62,9 @@ d <- data.frame(
 
 d |>
   e_charts(nam) |>
+  e_bar(AKM) |>
   e_bar(dem, name="Demand") |>
   e_bar(fin) |>
-  e_bar(AKM) |>
   e_bar(NPr) |>
 #  e_bar(mean_val, name="Mittelwert 2011 bis 2021:Q3") |>
  # e_line(Banken) |>
@@ -67,6 +75,6 @@ d |>
   e_title("Arbeitskräftemangel: Entwicklung und Satus Quo",subtext="Saisonbereinigte Werte",sublink="www.focus50plus.ch", left="10%") |>
   e_tooltip(trigger="axis") |>
   #e_datazoom(type = "slider") |>
-  e_legend(orient = 'horizontal', left = 420, top = 20) |>
+  e_legend(orient = 'horizontal', left = 320, top = 20) |>
   e_format_y_axis(suffix = "%") |>
   e_toolbox_feature("saveAsImage")
