17916d82bc4fb72d25749a3900558f278ed47f46
Simon Wey <simonwey@Simons-MacBook-Pro.local>
2021-12-06 12:43:58 +0100

    commit 6.12.21:12:43

--- Compare_FK_Mang.R
+++ Compare_FK_Mang.R
@@ -19,10 +19,11 @@ names(xlsx_exa) <- xlsx_exa[1,]
 xlsx_exa <- xlsx_exa[-1,]
 
 
-V20 <- c(as.numeric(tail(xlsx_exa$Grosshandel,n=1)),as.numeric(tail(xlsx_exa$Gastgewerbe,n=1)),as.numeric(tail(xlsx_exa$IT,n=1)),as.numeric(tail(xlsx_exa$Bau,n=1)), as.numeric(tail(xlsx_exa$MEM,n=1)), as.numeric(tail(xlsx_exa$Pharma,n=1)))
-V21 <- c(mean(as.numeric(xlsx_exa$Grosshandel)),mean(as.numeric(xlsx_exa$Gastgewerbe)),mean(as.numeric(xlsx_exa$IT)),mean(as.numeric(xlsx_exa$Bau)),mean(as.numeric(xlsx_exa$MEM)),mean(as.numeric(xlsx_exa$Pharma)))
-names <- c(as.character('Grosshandel'),as.character('Gastgewerbe'),as.character('IT'),as.character('Bau'),as.character('MEM-Industrie'),as.character('Pharma'))
-V21
+V20 <- c(as.numeric(tail(xlsx_exa$IT,n=1)),as.numeric(tail(xlsx_exa$Gesundheit,n=1)),as.numeric(tail(xlsx_exa$Gastgewerbe,n=1)),as.numeric(tail(xlsx_exa$Grosshandel,n=1)),as.numeric(tail(xlsx_exa$Finanzen,n=1)),as.numeric(tail(xlsx_exa$Bau,n=1)), as.numeric(tail(xlsx_exa$MEM,n=1)), as.numeric(tail(xlsx_exa$Pharma,n=1)))
+#V21 <- c(mean(as.numeric(xlsx_exa$IT)),mean(as.numeric(xlsx_exa$Gesundheit)),mean(as.numeric(xlsx_exa$Gastgewerbe)),mean(as.numeric(xlsx_exa$Grosshandel)),mean(as.numeric(xlsx_exa$Finanzen)),mean(as.numeric(xlsx_exa$Bau)),mean(as.numeric(xlsx_exa$MEM)),mean(as.numeric(xlsx_exa$Pharma)))
+V21 <- c(mean(as.numeric(head(xlsx_exa$IT,-1))),mean(as.numeric(head(xlsx_exa$Gesundheit,-1))),mean(as.numeric(head(xlsx_exa$Gastgewerbe,-1))),mean(as.numeric(head(xlsx_exa$Grosshandel,-1))),mean(as.numeric(head(xlsx_exa$Finanzen,-1))),mean(as.numeric(head(xlsx_exa$Bau,-1))),mean(as.numeric(head(xlsx_exa$MEM,-1))),mean(as.numeric(head(xlsx_exa$Pharma,-1))))
+names <- c(as.character('IT'),as.character('Gesundheit'),as.character('Gastgewerbe'),as.character('Grosshandel'),as.character('Finanzen'),as.character('Bau'),as.character('MEM-Industrie'),as.character('Pharma'))
+names
 
 d <- data.frame(
   nam=names,
@@ -38,9 +39,10 @@ d |>
  # e_line(Gastgewerbe) |>
  # e_line(Gesundheit) |>
  # e_line(IT) |>
-  e_title("Arbeitskräftemangel",subtext="Saisonbereinigte Werte",sublink="www.focus50plus.ch", left="10%") |>
+  e_x_axis(axisLabel = list(interval = 0, rotate = 45)) |> # rotate
+  e_title("Arbeitskräftemangel: Entwicklung und Satus Quo",subtext="Saisonbereinigte Werte",sublink="www.focus50plus.ch", left="10%") |>
   e_tooltip(trigger="axis") |>
   #e_datazoom(type = "slider") |>
-  e_legend(orient = 'horizontal', left = 460, top = 10) |>
+  e_legend(orient = 'horizontal', left = 320, top = 20) |>
   e_format_y_axis(suffix = "%") |>
   e_toolbox_feature("saveAsImage")
