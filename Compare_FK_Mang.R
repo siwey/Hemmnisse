e832e0e4c752fd5ee2d1705a8bb73d42d3a1ecfc
Simon Wey <simonwey@Simons-MacBook-Pro.local>
2021-12-07 22:28:06 +0100

    commit 07.12.21:22:28

--- Compare_FK_Mang.R
+++ Compare_FK_Mang.R
@@ -1,7 +1,6 @@
 rm(list=ls())
 
 setwd("/Users/simonwey/Repos/Hemmnisse")
-
 library(echarts4r)
 library(viridisLite)
 library(jsonlite)
@@ -19,9 +18,9 @@ names(xlsx_exa) <- xlsx_exa[1,]
 xlsx_exa <- xlsx_exa[-1,]
 
 
-V20 <- c(as.numeric(tail(xlsx_exa$IT,n=1)),as.numeric(tail(xlsx_exa$Gesundheit,n=1)),as.numeric(tail(xlsx_exa$Gastgewerbe,n=1)),as.numeric(tail(xlsx_exa$Grosshandel,n=1)),as.numeric(tail(xlsx_exa$Finanzen,n=1)),as.numeric(tail(xlsx_exa$Bau,n=1)), as.numeric(tail(xlsx_exa$MEM,n=1)), as.numeric(tail(xlsx_exa$Pharma,n=1)))
+V20 <- c(round(as.numeric(tail(xlsx_exa$IT,n=1)),0),round(as.numeric(tail(xlsx_exa$Gesundheit,n=1)),0),round(as.numeric(tail(xlsx_exa$Gastgewerbe,n=1)),0),round(as.numeric(tail(xlsx_exa$Grosshandel,n=1)),0),round(as.numeric(tail(xlsx_exa$Finanzen,n=1)),0),round(as.numeric(tail(xlsx_exa$Bau,n=1)),0),round(as.numeric(tail(xlsx_exa$MEM,n=1)),0),round(as.numeric(tail(xlsx_exa$Pharma,n=1)),0))
 #V21 <- c(mean(as.numeric(xlsx_exa$IT)),mean(as.numeric(xlsx_exa$Gesundheit)),mean(as.numeric(xlsx_exa$Gastgewerbe)),mean(as.numeric(xlsx_exa$Grosshandel)),mean(as.numeric(xlsx_exa$Finanzen)),mean(as.numeric(xlsx_exa$Bau)),mean(as.numeric(xlsx_exa$MEM)),mean(as.numeric(xlsx_exa$Pharma)))
-V21 <- c(mean(as.numeric(head(xlsx_exa$IT,-1))),mean(as.numeric(head(xlsx_exa$Gesundheit,-1))),mean(as.numeric(head(xlsx_exa$Gastgewerbe,-1))),mean(as.numeric(head(xlsx_exa$Grosshandel,-1))),mean(as.numeric(head(xlsx_exa$Finanzen,-1))),mean(as.numeric(head(xlsx_exa$Bau,-1))),mean(as.numeric(head(xlsx_exa$MEM,-1))),mean(as.numeric(head(xlsx_exa$Pharma,-1))))
+V21 <- c(round(mean(as.numeric(head(xlsx_exa$IT,-1))),0),round(mean(as.numeric(head(xlsx_exa$Gesundheit,-1))),0),round(mean(as.numeric(head(xlsx_exa$Gastgewerbe,-1))),0),round(mean(as.numeric(head(xlsx_exa$Grosshandel,-1))),0),round(mean(as.numeric(head(xlsx_exa$Finanzen,-1))),0),round(mean(as.numeric(head(xlsx_exa$Bau,-1))),0),round(mean(as.numeric(head(xlsx_exa$MEM,-1))),0),round(mean(as.numeric(head(xlsx_exa$Pharma,-1))),0))
 names <- c(as.character('IT'),as.character('Gesundheit'),as.character('Gastgewerbe'),as.character('Grosshandel'),as.character('Finanzen'),as.character('Bau'),as.character('MEM-Industrie'),as.character('Pharma'))
 names
 
@@ -42,6 +41,8 @@ d |>
   e_x_axis(axisLabel = list(interval = 0, rotate = 45)) |> # rotate
   e_title("Arbeitskräftemangel: Entwicklung und Satus Quo",subtext="Saisonbereinigte Werte",sublink="www.focus50plus.ch", left="10%") |>
   e_tooltip(trigger="axis") |>
+  #e_tooltip(trigger = 'axis',
+  #          format ="percent") |>
   #e_datazoom(type = "slider") |>
   e_legend(orient = 'horizontal', left = 320, top = 20) |>
   e_format_y_axis(suffix = "%") |>
